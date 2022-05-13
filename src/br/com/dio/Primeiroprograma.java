9363c68132124dacaeac2d1b50a9c853fb079b06
Rinaldo <rinaldotrade@gmail.com>
2022-05-12 23:43:38 -0300

    first commit

---
+++ src/br/com/dio/Primeiroprograma.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.model.Gata;
+
+public class Primeiroprograma {
+    public static void main(String[] args) {
+        Gata gata = new Gata();
+        System.out.println(gata);
+
+        /*float a = 5;
+          float b = 2;
+       System.out.println("hello world! " + (a/b));*/
+
+    }
+}
